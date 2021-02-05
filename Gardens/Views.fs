39eb3314e2da1bef3a1feab0a73014d1789bc182
Julia Pitts <julia@robo.church>
2021-02-04 23:28:13 -0800

    [Gardens] Tidied up the UI a bit.

--- Gardens/Views.fs
+++ Gardens/Views.fs
@@ -23,12 +23,18 @@ let index (garden : Model.Garden) =
     task {
         let! state = garden.GetState(None)
         return layout [
-            h1 [] [str "ASCII Garden"]
-            p [_id "age"] [str (sprintf "Age: %d ticks" state.Tick)]
-            p [_id "numPlants"] [str (sprintf "Plants: %d" state.NumPlants)]
-            p [_id "numWatchers"] [str (sprintf "Watchers: %d" (state.NumWatchers + 1))]
-            p [] [
-                pre [_id "garden"] [str (state.Garden.Value)]
+            div [_class "flex"] [
+                div [_class "spacer"] []
+                div [] [
+                    h1 [] [str "ASCII Garden"]
+                    div [_class "flex"] [
+                        div [_id "age"; _class "spacer left"] [str (sprintf "Age: %d ticks" state.Tick)]
+                        div [_id "numPlants"; _class "spacer center"] [str (sprintf "Plants: %d" state.NumPlants)]
+                        div [_id "numWatchers"; _class "spacer right"] [str (sprintf "Watchers: %d" (state.NumWatchers + 1))]
+                    ]
+                    pre [_id "garden"] [str (state.Garden.Value)]
+                ]
+                div [_class "spacer"] []
             ]
         ]
     }
